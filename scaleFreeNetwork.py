cccc9005f169a92ca2ea11c9a232ecce3025a3d4
SYYANI <wunongfeidao@live.com>
2020-10-25 01:35:40 +0800

    just good

--- scaleFreeNetwork.py
+++ scaleFreeNetwork.py
@@ -1,12 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 24 22:14:01 2020
+
+@author: sunyiyan
+"""
+
 class Nood:
     def __init__(self):
         self.connectNum = 0
-
+        self.linkNum = 0
+    def connectNood(self,nood):
+        self.linkNum += 1
+        nood.linkNum += 1
     def addNood(self, nood):
         self.connectNum += 1
         nood.connectNum += 1
-
+        
 def chooseNood(edges):
+    ranNum=random.randint(0,len(edges)-1)
+    return edges[ranNum]
+
+def chooseNood_2(edges):
     numOfEdges = len(edges)
     my_dict = dict()
     sum = 0
@@ -17,12 +32,27 @@ def chooseNood(edges):
     for j in range(numOfEdges):
         if my_dict[j][0]< ranNum <= my_dict[j][1]:
             return edges[j]
-
-def chooseNood_2(edges):
-    ranNum=random.randint(0,len(edges)-1)
-    return edges[ranNum]
-
+        
 def draw(edges):
+    numToconnection_dict = dict()
+    while edges:
+        lastNood = edges.pop()
+        if lastNood.linkNum in numToconnection_dict.keys():
+            numToconnection_dict[lastNood.linkNum] += 1
+        else:
+            numToconnection_dict[lastNood.linkNum] = 1
+        
+    plt.style.use('seaborn')
+    fig, ax = plt.subplots()
+
+    ax.set_title("Erdos-Renli Random Network",fontsize=24)
+    ax.set_xlabel("Number of connections",fontsize=14)
+    ax.set_ylabel("Number",fontsize=14)
+    ax.scatter(numToconnection_dict.keys(), numToconnection_dict.values(), s=30)
+    
+    plt.show()
+    
+def draw_2(edges):
     numToconnection_dict = dict()
     numToconnection_dict_key = dict()
     while edges:
@@ -44,46 +74,47 @@ def draw(edges):
     ax.set_xlabel("lg(Number of connections)",fontsize=14)
     ax.set_ylabel("lg(Number)",fontsize=14)
     ax.scatter(numToconnection_dict_key.values(), numToconnection_dict.values(), s=30)
-    
     plt.show()
-
-
-def draw_2(edges):
-    numToconnection_dict = dict()
-    numToconnection_dict_key = dict()
-    while edges:
-        lastNood = edges.pop()
-        if lastNood.connectNum in numToconnection_dict.keys():
-            numToconnection_dict[lastNood.connectNum] += 1
-        else:
-            numToconnection_dict[lastNood.connectNum] = 1
-        
-        
-
-    plt.style.use('seaborn')
-    fig, ax = plt.subplots()
-
-    ax.set_title("Scale Free Network",fontsize=24)
-    ax.set_xlabel("Number of connections",fontsize=14)
-    ax.set_ylabel("Number",fontsize=14)
-    ax.scatter(numToconnection_dict.keys(), numToconnection_dict.values(), s=30)
     
-    plt.show()
-
 if __name__ == "__main__":
     import random
     import math
     import matplotlib.pyplot as plt
-
+    
+    chooseNumber = int(input("\nchoose Erdos-Renyi model by 1 or Scale-Free model by 2: "))
+    
     edges = list()
     nood1, nood2 = Nood(), Nood()
     nood1.addNood(nood2)
     edges.append(nood1)
     edges.append(nood2)
+    
+    if (chooseNumber==1):
+        final_nodes = int(input("\nPlease type in the final number of nodes: "))
+        m_parameter = int(input("\nPlease type in the least number of each nood connected: "))
+        
+        for i in range(final_nodes-2):
+            new_nood = Nood()
+            edges.append(new_nood)
+        
+        for i in range(final_nodes):
+            for j in range(m_parameter):
+                chooseNood(edges).connectNood(edges[i])
+                
+        draw(edges)
+        
+    if (chooseNumber==2):
+        final_nodes = int(input("\nPlease type in the final number of nodes: "))
+        m_parameter = int(input("\nPlease type in the number of each nood connected when it first came into the network: "))
+        for i in range(final_nodes-2):
+            new_nood = Nood()
+            for j in range(m_parameter):
+                chooseNood_2(edges).addNood(new_nood)
+            edges.append(new_nood)
+        draw_2(edges)
 
-    for i in range(4999):
-        new_nood = Nood()
-        chooseNood_2(edges).addNood(new_nood)
-        edges.append(new_nood)
-
-    draw(edges)
+        
+        
+        
+        
+        
